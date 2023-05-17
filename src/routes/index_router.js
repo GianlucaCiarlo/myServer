4bc6b1d38c9ffef0855315a68e8ef6471b617d56
Gianluca Ciarlo <gianciarlo@gmail.com>
2023-05-16 22:41:46 -0300

    sprint-4

--- src/routes/index_router.js
+++ src/routes/index_router.js
@@ -1,17 +1,14 @@
 import { Router } from "express";
 import api_router from "./api/index.js";
-
+import viewsRouter from "../routes/views/productsView.js";
 
 
 const router = Router()
 
 router.use('/api', api_router)
-
+router.use ('/',viewsRouter)
 
 
 
 export default router
 
-// enrutador principal
-// + enrutador api
-// + enrutador carts
